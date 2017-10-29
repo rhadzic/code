54c6ea29c9f65961db403fead9b251ede551b090
Rijad Hadzic <rijad.hadzic@hotmail.com>
2017-10-29 13:40:47 +0100

    Creating entites in db

--- exampledb.sql
+++ exampledb.sql
@@ -40,6 +40,8 @@ CREATE TABLE IF NOT EXISTS `employee` (
 INSERT INTO `employee` (`id`, `emp_name`, `emp_designation`, `emp_salary`) VALUES
 (7, 'Uzumaki Naruto', 'Hokage', 125000),
 (8, 'Uchiha Sasuke', 'Rogeu Shinobi', 105000),
+(9, 'Uchiha Sasuke', 'Rogeu Shinobi', 105000),
+(11, 'Uchiha Sasuke', 'Rogeu Shinobi', 105000),
 (10, 'Monkey D. Luffy', 'Mugiwara Captain', 500000);
 
 --
@@ -64,3 +66,125 @@ ALTER TABLE `employee`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+--
+-- Table structure for table `student`
+--
+CREATE TABLE IF NOT EXISTS student (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+firstName varchar(32) not null,
+lastName varchar(32) not null,
+dateOfBirth date not null,
+gender varchar(32) not null,
+email varchar(128) not null,
+password varchar(32) not null,
+phoneNumber varchar(64) null,
+numberOfInstructions integer null
+  
+)ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `instructor`
+--
+CREATE TABLE IF NOT EXISTS instructor (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+firstName varchar(32) not null,
+lastName varchar(32) not null,
+dateOfBirth date not null,
+gender varchar(32) not null,
+email varchar(64) not null,
+iban varchar(128) not null,
+periodOfCancelation integer null,
+numberOfInstructions integer null,
+rate decimal(5,2) null
+  
+)ENGINE= InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `subject`
+--
+CREATE TABLE IF NOT EXISTS subject (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name varchar(32) not null,
+shortName varchar(16) null,
+numberOfBookedInstructions integer null,
+numberOfOfferedInstructions integer null  
+)ENGINE= InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `instruction`
+--
+CREATE TABLE IF NOT EXISTS instruction (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+instructorId int not null,
+studentId int not null,
+subjectId int not null,
+dateCreated date not null,
+dateConfirmed date null,
+datePayed date null,
+bruttoPrice decimal(6,2) not null
+)ENGINE= InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE instruction ADD CONSTRAINT FOREIGN KEY (instructorId) REFERENCES instructor(id);
+ALTER TABLE instruction ADD CONSTRAINT FOREIGN KEY (studentId) REFERENCES student(id);
+ALTER TABLE instruction ADD CONSTRAINT FOREIGN KEY (subjectId) REFERENCES subject(id);
+
+--
+-- Table structure for table `termin`
+--
+CREATE TABLE IF NOT EXISTS termin (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+instructorId int not null,
+subjectId int not null,
+terminDate DATE not null,
+terminTime varchar(32) not null,
+bruttoPrice decimal(6,2) not null,
+ageFrom integer not null,
+ageTo integer not null,
+available boolean not null DEFAULT true
+)ENGINE= InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE termin ADD CONSTRAINT FOREIGN KEY (instructorId) REFERENCES instructor(id);
+ALTER TABLE termin ADD CONSTRAINT FOREIGN KEY (subjectId) REFERENCES subject(id);
+
+--
+-- Table structure for table `rule`
+--
+CREATE TABLE IF NOT EXISTS rule (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+instructorId int not null,
+subjectId int not null,
+ageFrom Integer not null,
+ageTo Integer not null,
+daysInAdvance Integer not null,
+availableMonday boolean not null,
+availableTuesday boolean not null,
+availableWednesday boolean not null,
+availableThursday boolean not null,
+availableFriday boolean not null,
+availableSaturday boolean not null,
+availableSunday boolean not null,
+availableTime6 boolean not null,
+availableTime7 boolean not null,
+availableTime8 boolean not null,
+availableTime9 boolean not null,
+availableTime10 boolean not null,
+availableTime11 boolean not null,
+availableTime12 boolean not null,
+availableTime13 boolean not null,
+availableTime14 boolean not null,
+availableTime15 boolean not null,
+availableTime16 boolean not null,
+availableTime17 boolean not null,
+availableTime18 boolean not null,
+availableTime19 boolean not null,
+availableTime20 boolean not null,
+availableTime21 boolean not null,
+availableTime22 boolean not null,
+availableTime23 boolean not null
+)ENGINE= InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE rule ADD CONSTRAINT FOREIGN KEY (instructorId) REFERENCES instructor(id);
+ALTER TABLE rule ADD CONSTRAINT FOREIGN KEY (subjectId) REFERENCES subject(id);
+
+
